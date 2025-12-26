1358e1379f901d29582d266fd0a021f7448bfb32
Nikhil <nikhil14916@outlook.com>
2025-12-26 15:48:45 +1100

    Installed Chakra.
    Stripped the provided template code.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,52 +11,15 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Runner</title>
         <link rel="icon" href="./favicon.ico" />
       </Head>
 
 
-      <div
-        className={styles.page}
-      >
-        <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src={theBasePath + "/next.svg"}
-            alt="Next.js logo"
-            width={100}
-            height={20}
-            priority
-          />
-          <div className={styles.intro}>
-            <h1>To get started, edit the index.tsx file.</h1>
-            <p>
-              Looking for a starting point?
-            </p>
-          </div>
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https:google.com"
-            >
-              <Image
-                className={styles.logo}
-                src={theBasePath + "/vercel.svg"}
-                alt="Vercel logomark"
-                width={16}
-                height={16}
-              />
-              Deploy Now
-            </a>
-            <a
-              className={styles.secondary}
-              href="https:google.com"
-            >
-              Documentation
-            </a>
-          </div>
-        </main>
-      </div>
+
+      <main>
+      </main>
+
     </>
   );
 }

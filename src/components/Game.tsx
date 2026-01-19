26b555bb528becb0513af7511358c0aee10cb69d
Nikhil <nikhil14916@outlook.com>
2026-01-19 17:19:49 +1100

    Implemented a slider and passing of a value to the the requried component.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,5 @@
 import styles from "@/styles/Home.module.css";
-import { Button } from "@chakra-ui/react";
+import { Button, Slider, SliderFilledTrack, SliderMark, SliderThumb, SliderTrack } from "@chakra-ui/react";
 import { useState } from "react";
 import GameLogic from "@/components/GameLogic";
 
@@ -11,6 +11,10 @@ export default function GameComp() {
   const [showPauseButton, setShowPauseButton] = useState<boolean>(false);
   
   const [fullscreenRequested, setFullscreenRequested] = useState<boolean>(false);
+
+  const slider1defaultvalue = 3000;
+  const [slider1val, setSlider1Val] = useState<number>(slider1defaultvalue); // The value of the label.
+  const [slider1finalval, setSlider1FinalVal] = useState<number>(slider1defaultvalue); // The value once the slider has stopped being changed.
 
 
 
@@ -45,7 +49,33 @@ export default function GameComp() {
         {showPauseButton && <Button className={styles.pausebutton} onClick={handlePauseClick}>Pause</Button>}
       </div>
 
-      <GameLogic></GameLogic>
+      {/* Frequency of new bubbles being generated. */}
+      {showPlayButton && 
+        <Slider defaultValue={slider1defaultvalue} aria-label='slider-ex-1' step={1} 
+          min={250}
+          max={3000}
+          onChange={(val) => setSlider1Val(val)} 
+          onChangeEnd={(val) => setSlider1FinalVal(val)}>
+          <SliderMark
+            value={1500} // The position where the label displays.
+            textAlign='center'
+            bg='blue.500'
+            color='white'
+            mt='-10'
+            ml='-5'
+            w='12'
+          >
+            {slider1val}
+          </SliderMark>
+          <SliderTrack>
+            <SliderFilledTrack />
+          </SliderTrack>
+          <SliderThumb />
+        </Slider>
+      }
+
+      <GameLogic bubbleFrequency={slider1finalval}></GameLogic>
+
 
     </div>
   );
